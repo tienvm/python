7eccac86a7dbe1836eef54dd12733c2037d92c90
tienvm <minhtien2705@gmail.com>
2015-09-19 09:19:10 +0700

    random

--- randomBarca.py
+++ randomBarca.py
@@ -8,14 +8,15 @@ HauVeBien = ['BARTRA', 'DOUGLAS', 'J.ALBA', 'MATHIEU']
 TienVe = ['RAKITIC', 'BUSQUETS', 'INIESTA', 'RAFINHA', 'RAMÍREZ', 'S.ROBERTO', 'DEULOFEU', 'A.TURAN']
 TienDao = ['SUÁREZ', 'L.MESSI', 'NEYMAR', 'HADDADI', 'S.RAMIREZ']
 
-BDoiHinhRaSan = ThuMon + HauVeTru + HauVeBien + TienDao + TienVe
 BThuMon = random.sample(ThuMon, 1)
-print('THU MON: %30s ' % list(BThuMon))
 BHauVeTru = random.sample(HauVeTru, 2)
-print('HAU VE TRU: %30s ' % list(BHauVeTru))
 BHauVeBien = random.sample(HauVeBien, 2)
-print('HAU VE BIEN: %30s' % list(BHauVeBien))
 BTienVe = random.sample(TienVe, 4)
-print('TIEN VE: %20s' % list(BTienVe))
 BTienDao = random.sample(TienDao, 2)
-print('TIEN DAO: %30s' % list(BTienDao))
+
+BDoiHinhRaSan = BThuMon + BHauVeTru + BHauVeBien + BTienVe + BTienDao
+
+print('THU MON: %30s ' % list(BThuMon))
+print('HAU VE TRU: %30s ' % list(BHauVeTru))
+print('HAU VE BIEN: %30s' % list(BHauVeBien))
+print('TIEN VE: %20s' % list(BTienVe))
